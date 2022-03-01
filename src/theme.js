44e38e58f83b83f804012a213eb0d8b764827264
AlisonLetos <alisonletos@hotmail.com>
2022-03-01 12:28:28 -0300

    estrutura inicial + material.ui

---
+++ src/theme.js
@@ -0,0 +1,12 @@
+// import para criar o tema personalizado
+import { createTheme } from "@material-ui/core/styles";
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#000000',
+        }
+    }
+})
+
+export default theme
